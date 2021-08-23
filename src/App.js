4eb9e6035b05fdc1bfa6d0e0c692e78289bdc83a
William <williamallen12364@gmail.com>
2021-08-23 22:20:30 +0700

    added links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <h1><Home /></h1>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,13 @@
-import { useState } from "react"
 import BlogList from "./BlogList"
+import useFetch from "./useFetch"
 const Home = () => {
-   const [blogs, setBlog] = useState([
-      { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-      { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-      { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-   ])
-   const handleDelete = (id) => {
-      const newBlogs = blogs.filter((blog) => blog.id !== id)
-      setBlog(newBlogs)
-   }
+   const {data, isPending, error} = useFetch('http://localhost:8000/blogs')
 
    return (
       <div className="home">
-         <BlogList blogs={blogs} handleDelete={handleDelete} title='All Blogs!' />
-         <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} handleDelete={handleDelete} title="Mario's Blogs!" />
+         {error && <div>{error}</div>}
+         {isPending && <div>Loading...</div>}
+         {data && <BlogList blogs={data} title='All Blogs!' />}
       </div>
    );
 }
