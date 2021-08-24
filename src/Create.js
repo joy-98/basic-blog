81c9bf97c78c2321271ffb73ba8c070595983b6c
William <williamallen12364@gmail.com>
2021-08-24 17:08:26 +0700

    added form

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,49 @@
+import { useState } from "react";
 const Create = () => {
-    return ( 
+    // alternative way to make useState
+    const [state, setState] = useState({
+        title: '',
+        body: '',
+        author: 'mario'
+    })
+
+    const handleChange = (evt) => {
+        setState({
+            ...state,
+            [evt.target.name]: evt.target.value
+        })
+    }
+
+    return (
         <div className="create">
-            <h1>Add a new Blog!!</h1>
+            <form>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    name='title'
+                    required
+                    value={state.title}
+                    onChange={handleChange}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    name='body'
+                    required
+                    value={state.body}
+                    onChange={handleChange}
+                />
+                <label>Blog author:</label>
+                <select
+                    name='author'
+                    onChange={handleChange}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
-     );
+    );
 }
- 
+
 export default Create;
