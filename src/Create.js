7a396152ffea43fbdd155e541dfb6a77fd932199
William <williamallen12364@gmail.com>
2021-08-25 15:26:45 +0700

    added post method

--- src/Create.js
+++ src/Create.js
@@ -3,11 +3,21 @@ const Create = () => {
     const [title, setTitle] = useState("")
     const [body, setBody] = useState("")
     const [author, setAuthor] = useState('mario')
+    const [isPending, setIsPending] = useState(false)
 
     const handleSubmit = (evt) => {
         evt.preventDefault()
-        const blog = {title, body, author}
-        console.log(blog)
+        const blog = { title, body, author }
+        setIsPending(true)
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log('new blog added')
+            setIsPending(false)
+        })
     }
 
     return (
@@ -33,7 +43,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog</button>}
             </form>
         </div>
     );
